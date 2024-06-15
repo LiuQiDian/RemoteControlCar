32e236907e2b286c333c9f82683e585ac3d99c86
LiuQiDian <3230643253@qq.com>
2024-06-15 21:46:18 +0800

    蓝牙代码框架

--- Core/Inc/bluetooth.h
+++ Core/Inc/bluetooth.h
@@ -8,10 +8,17 @@ extern "C"
 
 #include "main.h"
 #include "usart.h"
+#define USART1_RX_LEN 200
+
+    extern UART_HandleTypeDef huart1;
+    extern uint8_t USART1_RX_BUF[USART1_RX_LEN]; // 串口接收缓冲,最大USART1_RX_LEN个字节.
+    extern uint16_t USART1_RX_STA;               // 接收状态标记
+    extern uint8_t USART1_NeData;                // 最新接收到的数据
 
     void BLUETOOTH_Init();
     void BLUETOOTH_Send(uint8_t *data, uint16_t size);
     void BLUETOOTH_Receive(uint8_t *data, uint16_t size);
+    void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
 
 #ifdef __cplusplus
 }
--- Core/Src/bluetooth.c
+++ Core/Src/bluetooth.c
@@ -1,5 +1,9 @@
 #include "bluetooth.h"
 
+uint8_t USART1_RX_BUF[USART1_RX_LEN]; // 串口接收缓冲,最大USART1_RX_LEN个字节.
+uint16_t USART1_RX_STA = 0;           // 接收状态标记
+uint8_t USART1_NeData;                // 最新接收到的数据
+
 // 蓝牙初始化
 void BLUETOOTH_Init()
 {
@@ -16,4 +20,34 @@ void BLUETOOTH_Send(uint8_t *data, uint16_t size)
 void BLUETOOTH_Receive(uint8_t *data, uint16_t size)
 {
     HAL_UART_Receive(&huart1, data, size, 1000);
+}
+
+// 串口接收中断
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    if (huart->Instance == USART1)
+    {
+        if ((USART1_RX_STA & 0x8000) == 0)
+        {
+            if (USART1_RX_STA & 0x4000)
+            {
+                if (USART1_NeData != 0x0a)
+                    USART1_RX_STA = 0;
+                else
+                    USART1_RX_STA |= 0x8000;
+            }
+            else
+            {
+                if (USART1_NeData == 0x0d)
+                    USART1_RX_STA |= 0x4000;
+                else
+                {
+                    USART1_RX_BUF[USART1_RX_STA & 0X3FFF] = USART1_NeData;
+                    USART1_RX_STA++;
+                    if (USART1_RX_STA > (USART1_RX_LEN - 1))
+                        USART1_RX_STA = 0;
+                }
+            }
+        }
+    }
 }
